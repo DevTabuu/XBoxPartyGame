669d35ee40157ed1f5d02d8de0f4d0be75979047
Youri van den Broek <521251@student.glu.nl>
2018-11-07 08:55:25 +0100

    Added Jette's Models

---
+++ Assets/Minigames/Party Popper/Scripts/LevelSwitch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSwitch : MonoBehaviour {
+
+    private int _LevelIndex;
+
+    [SerializeField]
+    private List<GameObject> _Levels;
+
+	public void SwitchLevel()
+    {
+        for(int i = 0; i < _Levels.Count; i++)
+        {
+
+        }
+    }
+}
